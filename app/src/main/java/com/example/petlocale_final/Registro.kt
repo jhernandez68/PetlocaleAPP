6bf22a24ad5c70b985f8c353f30aeee764140754
JHernandez-68 <56227740+JHernandez-68@users.noreply.github.com>
2022-10-24 17:00:51 -0500

    Manejo de excepciones al registrar usuario

--- app/src/main/java/com/example/petlocale_final/Registro.kt
+++ app/src/main/java/com/example/petlocale_final/Registro.kt
@@ -3,9 +3,12 @@ package com.example.petlocale_final
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import com.google.firebase.FirebaseNetworkException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
 import kotlinx.android.synthetic.main.activity_registro.*
 
 class Registro : AppCompatActivity() {
@@ -30,23 +33,53 @@ class Registro : AppCompatActivity() {
 
                 if(editTextTextPassword.text.toString() == editTextTextPassword2.text.toString()){
 
-                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(editTextTextEmailAddress.text.toString()
-                        , editTextTextPassword.text.toString()).addOnCompleteListener{
-                        if(it.isSuccessful){
-                            startActivity(Intent(this, Logeo::class.java))
-                        }else{
-                            showAlert()
+                    //Se verifica que la contraseña tenga al menos 6 dígitos
+                        if(editTextTextPassword.text.toString().length > 5){
+                            FirebaseAuth.getInstance()
+                                .createUserWithEmailAndPassword(editTextTextEmailAddress.text.toString()
+                                    , editTextTextPassword.text.toString()).addOnCompleteListener{
+                                    if(it.isSuccessful){
+                                        startActivity(Intent(this, Logeo::class.java))
+                                    }
+                                }.addOnFailureListener{
+                                    exception ->
+                                    handleException(exception)
+
+                                    showAlert(handleException(exception))
+
+                                    Log.d("Datos", "${handleException(exception)}")
+                                }
                         }
+                    if(editTextTextPassword.text.toString().length < 6){
+                        Toast.makeText(this, "La contraseña es muy corta!", Toast.LENGTH_LONG).show()
                     }
+
                 }
             }
 
         }
     }
-    private fun showAlert(){
+
+    private fun handleException(exception: Exception?): String{
+        //exception is null
+        when(exception){
+            is FirebaseAuthUserCollisionException -> return "Correo registrado"
+            is FirebaseNetworkException -> return "Revisa tu conexión a internet"
+            else -> return "Error producido al registrar el usuario"
+        }
+    }
+
+
+    private fun showAlert( mensaje : String){
         val builder = AlertDialog.Builder(this)
         builder.setTitle("Error")
-        builder.setMessage("Error producido al registrar el usuario")
+
+        if(mensaje == null || mensaje == "null"){
+            builder.setMessage("Error producido al registrar el usuario")
+        }else{
+            builder.setMessage(mensaje)
+
+        }
         builder.setPositiveButton("Aceptar", null)
         val dialog: AlertDialog = builder.create()
         dialog.show()
